b4e59542648eb998d4dfb5a8f6690541038bda3d
Vorobole <vorobyov1903@gmail.com>
2024-04-11 09:55:46 +0300

    fix movieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,17 +1,30 @@
-import { Link, useLocation } from 'react-router-dom';
-import css from './MovieList.module.css';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import css from "./MovieList.module.css";
 
 function MovieList({ movies }) {
-  const location = useLocation();
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (!movies || movies.length === 0) {
+        setShowMessage(true);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [movies]);
+
+  if (showMessage) {
+    return <div>No movies available</div>;
+  }
 
   return (
     <div className={css.container}>
       <ul className={css.list}>
-        {movies.map(movie => (
+        {movies.map((movie) => (
           <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`} state={location}>
-              {movie.title}
-            </Link>
+            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
           </li>
         ))}
       </ul>
